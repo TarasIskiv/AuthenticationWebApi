65e9fe173e556bb5d8be13a365f0f8d2d1bdfe99
Taras <taras.iskiv@gmail.com>
2022-01-22 21:40:01 +0100

    Added classes and configurations to work with db and jwt token

---
+++ Data/AuthenticationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using AuthenticationWebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationWebApi.Data
+{
+    public class AuthenticationDBContext : DbContext
+    {
+        //DbSet
+        public AuthenticationDBContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+       public DbSet<User> Users { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,10 @@
+using AuthenticationWebApi.Data;
+using AuthenticationWebApi.JWT;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +29,12 @@ namespace AuthenticationWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AuthenticationDBContext>(options =>
+                            options.UseSqlServer(Configuration.GetConnectionString("ConnectionCS")));
 
+            var jwtSection = Configuration.GetSection("JWTSettings");
+            services.Configure<JWTSettings>(jwtSection);
+                
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
